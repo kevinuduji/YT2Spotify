c8b8300d16b6eadcbf35191fa802e66c1bec687a
Kevin Uduji <118309007+kevinuduji@users.noreply.github.com>
2023-10-27 21:08:09 -0500

    Initial Upload of files

---
+++ secret.py
@@ -0,0 +1,9 @@
+# For the YouTube Data API
+PLAYLIST_ID = ""
+
+# For the Spotify Web API
+CLIENT_ID = "5ac9ab23096a417084061c95e3e964f7"
+CLIENT_SECRET = "a3b4fbc9c1c641f29f9efa7d7ffd6c50"
+USER_ID = "kevinuduji"
+REDIRECT_URI = "https://www.google.com"
+PLAYLIST_ID = "PLMBrY5tlAwTBzsdgf4iwKP2E4bz6IjKP8"
